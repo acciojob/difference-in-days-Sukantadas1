1658eb552f6a2f9e963341f450f333c90accd462
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-16 15:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+ const oneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
+
+  // Parse the input dates
+  const parsedDate1 = new Date(date1);
+  const parsedDate2 = new Date(date2);
+
+  // Calculate the time difference between the two dates
+  const timeDiff = parsedDate2 - parsedDate1;
+
+  // Calculate the difference in days
+  const diffDays = Math.round(timeDiff / oneDay);
+
+  return diffDays;
 };
 
 // Do not change the code below.
